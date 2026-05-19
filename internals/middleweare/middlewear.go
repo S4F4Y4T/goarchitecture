95f4dbfafa80793869fc8a6d57d824f7140e7544
s4f4y4t <safayat.me@gmail.com>
2026-05-19 15:47:55 +0600

    Add Middleware struct and update Register function to use middleware logger

--- internals/middleweare/middlewear.go
+++ internals/middleweare/middlewear.go
@@ -1,1 +1,4 @@
 package middleweare
+
+type Middleware struct {
+}
--- router/router.go
+++ router/router.go
@@ -2,13 +2,14 @@ package router
 
 import (
 	"microservice/internals/bootstrap"
+	"microservice/internals/middleweare"
 	"net/http"
 )
 
-func Register(handler *bootstrap.App) *http.ServeMux {
+func Register(handler *bootstrap.App) http.Handler {
 	mux := http.NewServeMux()
 
 	RegisterUsersRoute(mux, handler.UserHandler)
 
-	return mux
+	return middleweare.Logger(mux)
 }
