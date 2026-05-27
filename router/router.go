83a2e684287e2fcc035c258332607958a250eb92
s4f4y4t <safayat.me@gmail.com>
2026-05-27 15:59:09 +0600

    Added Swagger

--- router/router.go
+++ router/router.go
@@ -1,15 +1,24 @@
 package router
 
 import (
+	"microservice/docs"
 	"microservice/internals/bootstrap"
 	"microservice/internals/middleweare"
 	"net/http"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 func Register(handler *bootstrap.App) http.Handler {
 	mux := http.NewServeMux()
 
 	RegisterUsersRoute(mux, handler.UserHandler)
+
+	mux.HandleFunc("GET /swagger/openapi.yaml", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/yaml")
+		w.Write(docs.SpecYAML)
+	})
+	mux.Handle("/swagger/", httpSwagger.Handler(httpSwagger.URL("/swagger/openapi.yaml")))
 
 	return middleweare.Chain(middleweare.Logger, middleweare.Cors, middleweare.PanicRecovery)(mux)
 }
--- router/user.go
+++ router/user.go
@@ -8,11 +8,11 @@ import (
 func RegisterUsersRoute(mux *http.ServeMux, handler *handler.UserHandler) *http.ServeMux {
 	userMux := http.NewServeMux()
 
-	userMux.HandleFunc("GET /", handler.GetUserByID)
+	userMux.HandleFunc("GET /", handler.GetAllUsers)
 	userMux.HandleFunc("GET /{id}", handler.GetUserByID)
-	userMux.HandleFunc("POST /", handler.GetUserByID)
-	userMux.HandleFunc("PUT /{id}", handler.GetUserByID)
-	userMux.HandleFunc("DELETE /{id}", handler.GetUserByID)
+	userMux.HandleFunc("POST /", handler.CreateUser)
+	userMux.HandleFunc("PUT /{id}", handler.UpdateUser)
+	userMux.HandleFunc("DELETE /{id}", handler.DeleteUser)
 
 	mux.Handle("/users/", http.StripPrefix("/users", userMux))
 	return mux
